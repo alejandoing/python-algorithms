55661fb23014f034bbf20863d0113eaa048561c5
Alejandro Uray <urayalejandro@gmail.com>
2018-02-22 18:09:28 -0300

    First commit

---
+++ main.py
@@ -0,0 +1,23 @@
+def run():
+	print('''  
+      __^__                                      __^__
+     ( ___ )------------------------------------( ___ )
+      | / |                                      | \ |
+      | / |          Python Algorithms           | \ |
+      |___|                                      |___|
+     (_____)------------------------------------(_____) 
+        ''')
+	print('Selecciona un programa que quieras ejecutar:  \ ')
+	print('[1] Criptógrafo')
+	print('[2] Segundo programa')
+	print('[3] Tercer programa')
+	print('[4] Cuarto programa')
+	print('[5] Quinto programa')
+	print('[6] Sexto programa')
+	print('[7] Septimo programa')
+	print('[8] Octavo programa')
+	print('[9] Noveno programa')
+	print('[10] Decimo primer programa')
+
+if __name__ == '__main__':
+	run()
