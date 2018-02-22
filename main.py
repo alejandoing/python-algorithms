49a70ce183c4e0132b4568bc77d6603575468597
Alejandro Uray <urayalejandro@gmail.com>
2018-02-22 19:23:59 -0300

    Changed welcome ASCII message

--- main.py
+++ main.py
@@ -3,21 +3,9 @@ def run():
       __^__                                      __^__
      ( ___ )------------------------------------( ___ )
       | / |                                      | \ |
-      | / |          Python Algorithms           | \ |
-      |___|                                      |___|
+      | / |   Welcome to the Python Algorithms   | \ |
+      |___|           by Alejandoing             |___|
      (_____)------------------------------------(_____) 
         ''')
-	print('Selecciona un programa que quieras ejecutar:  \ ')
-	print('[1] Criptógrafo')
-	print('[2] Segundo programa')
-	print('[3] Tercer programa')
-	print('[4] Cuarto programa')
-	print('[5] Quinto programa')
-	print('[6] Sexto programa')
-	print('[7] Septimo programa')
-	print('[8] Octavo programa')
-	print('[9] Noveno programa')
-	print('[10] Decimo primer programa')
-
 if __name__ == '__main__':
 	run()
