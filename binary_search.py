20e01ebda46ecba4b8d45809c888643a5982552a
Alejandro Uray <urayalejandro@gmail.com>
2018-03-06 19:11:07 -0300

    Binary Search with random numbers and timer

--- binary_search.py
+++ binary_search.py
@@ -1,5 +1,10 @@
+import random
+import time
+
 class BinarySearch:
 	def __init__(self):
+		self.numbers = []
+		self.start_time = None
 		print('''
 			__________________________________________________________________
 			______ _                          _____                     _     
@@ -8,16 +13,59 @@ class BinarySearch:
 			| ___ \ | '_ \ / _` | '__| | | |  `--. \/ _ \/ _` | '__/ __| '_ \ 
 			| |_/ / | | | | (_| | |  | |_| | /\__/ /  __/ (_| | | | (__| | | |
 			\____/|_|_| |_|\__,_|_|   \__, | \____/ \___|\__,_|_|  \___|_| |_|
-			                           __/ |                                  
-			                          |___/                                   
+									   __/ |                                  
+									  |___/                                   
 			__________________________________________________________________
 		''')
 	
 	def start(self):
 		while True:
+			print(''' 
+				Select a option:
+
+				[1]: Random numbers
+				[2]: Manual numbers
+				[3]: Back to main menu
+				[4]: Exit
+			''')
 			try:
-				option = int(input('\nIngresa un número: '))
-				break
+				method = int(input("\nYour option: "))
+				if method == 1: 
+					self.random_numbers()
+					while True:
+						number = int(input("\nWrite a number: "))
+						self.start_time = time.time()
+						self.search(number, 0, len(self.numbers) - 1)
+				self.numbers = []
 			except ValueError:
-				print('\nCommand not found')
+				print("\nCommand not found")
 				continue
+
+	def random_numbers(self):
+		self.numbers = sorted(set([random.randint(1,10001) for x in range(random.randint(1,10001))]))
+		return print("\nA collection of {} numbers has been generated".format(len(self.numbers)))
+
+	def search(self, number_to_find, low, high):
+		elapsed_time = round(time.time() - self.start_time, 5)
+		mid = (low + high) // 2
+		
+		if low > high: 
+			return self.print_result(False, 'Number not found', elapsed_time)
+
+		if self.numbers[mid] == number_to_find: 
+			return self.print_result(True, 'Number found', elapsed_time)
+
+		elif self.numbers[mid] > number_to_find:
+			print("{} is less than {}".format(number_to_find, self.numbers[mid]))
+			return self.search(number_to_find, low, mid - 1)
+
+		else:
+			print("{} is greater than {}".format(number_to_find, self.numbers[mid]))
+			return self.search(number_to_find, mid + 1, high)
+
+	def print_result(self, result, message, time):
+		if time == 0.0: 
+			print("\n{} without delay!".format(message, time))
+		else:
+			print("\n{} with {} seconds of delay!".format(message, time))
+		return result
