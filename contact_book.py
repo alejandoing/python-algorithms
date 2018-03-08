e17fad0be22344be4137604725a3c4ee90c2a28e
Alejandro Uray <urayalejandro@gmail.com>
2018-03-08 18:15:02 -0300

    Created methods for to search and update contacts

--- contact_book.py
+++ contact_book.py
@@ -2,9 +2,9 @@ import sys
 
 class Contact:
 	def __init__(self, name, phone, email):
-		self.name = name
-		self.phone = phone
-		self.email = email
+		self.name = name.strip()
+		self.phone = phone.strip()
+		self.email = email.strip()
 
 class ContactBook:
 	def __init__(self):
@@ -40,9 +40,9 @@ class ContactBook:
 				if method == 1: 
 					self.add()
 				elif method == 2: 
-					pass
+					self.update()
 				elif method == 3: 
-					pass
+					self.search()
 				elif method == 4: 
 					pass
 				elif method == 5: 
@@ -66,7 +66,65 @@ class ContactBook:
 		self._contacts.append(Contact(name, phone, email))
 
 	def show_all(self):
-		[self._print_contact(contact) for contact in self._contacts]
+		contacts = [self._print_contact(contact) for contact in self._contacts]
+		if not len(contacts): print('\nThere are no added contacts')
+
+	def update(self):
+		contact_old = self.search()
+		if contact_old:
+			name = contact_old.name
+			phone = contact_old.phone
+			email = contact_old.email
+
+			while True:
+				print(''' 
+					Update contact:
+
+					[1]: Name
+					[2]: Phone
+					[3]: Email
+					[4]: All information
+				''')
+
+				try:
+					method = int(input("\nYour option: "))
+					if method == 1: 
+						name = str(input('\nWrite a name: '))
+					elif method == 2:
+						phone = str(input('\nWrite a phone: '))
+					elif method == 3:
+						email = str(input('\nWrite a email: '))
+					elif method == 4:
+						name = str(input('\nWrite a name: '))
+						phone = str(input('\nWrite a phone: '))
+						email = str(input('\nWrite a email: '))
+					else:
+						print("\nCommand not found")
+						continue
+
+					contact_new = Contact(name, phone, email)
+					
+					for idx, contact in enumerate(self._contacts):
+						if contact.name.lower() == contact_old.name.lower():
+							self._contacts[idx] = contact_new
+							self._print_contact(contact_new)
+							break
+					break
+
+				except ValueError:
+					print("\nCommand not found")
+					continue
+
+
+	def search(self):
+		name = str(input('\nWrite a name: ')).strip()
+		for contact in self._contacts:
+			if contact.name.lower() == name.lower():
+				self._print_contact(contact)
+				return contact
+		else:
+			print('\nContact not found')
+			return False
 
 	def _print_contact(self, contact):
 		print('\n--- * --- * ---- * ---- * ---- * ---- * ----')
