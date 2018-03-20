e71e5ef950ebe2cf4b4fcc1dee23738266735adc
Alejandro Uray <urayalejandro@gmail.com>
2018-03-20 17:19:18 -0300

    Completed the game cycle

--- hanged.py
+++ hanged.py
@@ -71,37 +71,19 @@ class Hanged:
         self.tries = 0
         self.word = None
         self.hidden_word = None
+        self.current_letter = None
+        self.letters_used = list()
 
     def start(self):
         self.word = self.random_word()
         self.hidden_word = ['-'] * len(self.word)
         while True:
             self.display_board()
-            current_letter = str(input('Write a letter: '))
-            self.tries += 1
-            # try:
-            #     method = int(input("\nYour option: "))
-            #     if method == 1: 
-            #         pass
-            #     elif method == 2: 
-            #         pass
-            #     elif method == 3:
-            #         break
-            #     elif method == 4:
-            #         sys.exit()
-            #     else:
-            #         print("\nCommand not found")
-            #         continue
-                
-            #     # while True:
-            #     #   number = int(input("\nWrite a number: "))
-            #     #   self.start_time = time.time()
-            #     #   self.search(number, 0, len(self.numbers) - 1)
-            #     #   break
-
-            # except ValueError:
-            #     print("\nCommand not found")
-            #     continue
+            self.current_letter = str(input('Write a letter: '))
+            if not self.correct_letter(self.current_letter): self.tries += 1
+            if self.game_over():
+                question = "\n Do you want play again? (Y/N): "
+                if not self.choose_yes_or_not(question): break
 
     def random_word(self):
         index = random.randint(0, len(self.words) - 1)
@@ -111,3 +93,54 @@ class Hanged:
         print(self.images[self.tries])
         print("\n {}".format(self.hidden_word))
         print('--- * --- * --- * --- * --- * ---')
+
+    def correct_letter(self, current_letter):
+        correct = False
+        for index in range(len(self.word)):
+            if self.word[index] == self.current_letter:
+                self.hidden_word[index] = self.current_letter
+                correct = True
+        return correct
+
+    def has_won(self):
+        if not '-' in self.hidden_word:
+            self.display_board()
+            self.send_message('Congratulations! You have won with a total of {} failures!'.format(self.tries))
+            return True
+
+    def has_lost(self):
+        if self.tries == 6:
+            self.display_board()
+            self.send_message('You lost! The word was {}'.format(self.word))
+            return True
+
+    def game_over(self):
+        self.set_status()
+        if self.has_won() or self.has_lost(): return True
+
+    def send_message(self, message):
+        print('\n {}'.format(message))
+
+    def set_status(self):
+        if not self.current_letter in self.letters_used: self.letters_used.append(self.current_letter)
+        self.send_message('Letters used: {}'.format(self.letters_used))
+        self.send_message('Tries: {}'.format(len(self.letters_used)))
+        if self.tries == 5: self.send_message('WARNING! You have one last attempt.')
+
+    def reset_game(self):
+        self.tries = 0
+        self.current_letter = None
+        self.letters_used = list()
+        self.word = self.random_word()
+        self.hidden_word = ['-'] * len(self.word)
+
+    def choose_yes_or_not(self, question):
+        while True:
+            answer = str(input(question)).lower()
+            if answer == 'n': return False
+            elif answer == 'y':
+                self.reset_game()
+                return True
+            else:
+                print("\n Command not found")
+                continue
