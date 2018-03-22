b180b58d8a5bfb69a0dec79b12d0b8375d726657
Alejandro Uray <urayalejandro@gmail.com>
2018-03-21 23:54:08 -0300

    Created method for to get pending

--- linear_regression.py
+++ linear_regression.py
@@ -29,7 +29,7 @@ class LinearRegression:
             ''')
 
             try:
-                method = int(input("\nYour option:"))
+                method = int(input("\nYour option: "))
                 if method == 1:
                     self.random_numbers()
                 elif method == 2: 
@@ -41,22 +41,22 @@ class LinearRegression:
                 else:
                     raise ValueError
                 
-                self.print_numbers()
+                pending = self.get_pending(self.numbers)
+                #self.get_point_intersection(pending)
 
             except ValueError:
                 print("\nCommand not found")
                 continue
 
     def random_numbers(self):
-        self.numbers = { random.randint(1,1001): random.randint(1,1001) for x in range(random.randint(2,10)) }
+        self.numbers = { x: random.randint(1,1001) for x in range(random.randint(1,10)) }
         return self.numbers
 
     def manual_numbers(self):
         self.numbers = {}
         try:
-            keys = list((map(int, str(input("\nWrite keys separated by comma: ")).split(','))))
             values = list((map(int, str(input("\nWrite values separated by comma: ")).split(','))))
-            self.numbers = {key: values[idx] for idx, key in enumerate(keys)}
+            self.numbers = {idx + 1: value for idx, value in enumerate(values)}
             return self.numbers
         
         except ValueError:
@@ -72,3 +72,15 @@ class LinearRegression:
     def print_numbers(self):
         print("\nGenerated Sample:")
         {print("\n{} => {}".format(key, val)) for key, val in self.numbers.items()}
+
+    def get_pending(self, sample):
+        future = int(input("\nWrite a index of prediction "))
+        self.print_numbers()
+        summatory_key_x_val = sum([(key * val) for key, val in sample.items()])
+        summatory_val = sum(sample.values())
+        summatory_key = sum(sample.keys())
+        summatory_key_square = sum([key ** 2 for key in sample.keys()])
+        dividend = (len(sample) * summatory_key_x_val) - (summatory_val * summatory_key)
+        divider = (len(sample) * summatory_key_square) - (summatory_key ** 2)
+        pending = dividend / divider
+        return pending
